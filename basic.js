64881301ee212a0bad93807348a45f4cc1f70c85
khushi964 <rakasha180806@gmail.com>
2024-12-17 07:28:26 +0000

    datatypes and basic

---
+++ basic.js
@@ -0,0 +1,8 @@
+console.log("hello khushi");
+const accountId = 144354;
+let a = "khushi";
+console.log(accountId);
+console.table([accountId,a]);
+/* prefer not to use var */
+let account;
+console.log([account,accountId,a]);
---
+++ datatypes.js
@@ -0,0 +1,11 @@
+"use strict";//treat all js code in newer version
+//alert(3+3)//we are using node.js not a browser
+let age = 18
+let name = "khushi"
+let loggedIn = true
+//number=>2 to power 53
+//null => standalone value
+//undefined => 
+// symbol => unique
+//object
+console.log(typeof null);    
