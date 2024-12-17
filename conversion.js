ba408c05c8f49f7f8e49783170c57a18a39f7a8c
khushi964 <rakasha180806@gmail.com>
2024-12-17 08:03:03 +0000

    conversion and opertions

---
+++ conversion.js
@@ -0,0 +1,27 @@
+let score = "33abc";
+console.log(typeof score);
+let num = Number(score);
+console.log(typeof num);
+console.log(num);// it will give output of NaN not a number 
+// "33" => 33
+//"33adc" => NaN
+//null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+// " " => false
+// "khushi" => true
+//String //Number // Boolean 
+//..........operations.......//
+let value =3
+let str1 = "hello "
+let str2 = "khushi"
+console.log(str1+str2);//hello khushi
+console.log("1"+2); // output will be 12
+console.log(1+"2"); //output will be 12
+console.log("1"+"2");//output will be 12
+//comparison 
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+
